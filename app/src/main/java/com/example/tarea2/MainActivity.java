f8efcbe036a5b79d0d7feb9bf71284da13b935d7
Junior Samuel De Los Santos Velazquez <juniorvelazquezmt19@gmail.com>
2022-01-26 21:46:03 -0400

    Personalización de los colores y corrección de String en el archivo del mismo nombre. Otros ajustes secundarios

--- app/src/main/java/com/example/tarea2/MainActivity.java
+++ app/src/main/java/com/example/tarea2/MainActivity.java
@@ -7,12 +7,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.text.DecimalFormat;
-
 public class MainActivity extends AppCompatActivity {
 
     private EditText input;
-    private TextView labelResul;
+    private TextView labelResult;
     private TextView labelError;
     private TextView result;
 
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         input = (EditText) findViewById(R.id.inputNumber);
         result = (TextView) findViewById(R.id.result);
-        labelResul = (TextView) findViewById(R.id.labelResul);
+        labelResult = (TextView) findViewById(R.id.labelResult);
         labelError = (TextView) findViewById(R.id.labelError);
     }
 
@@ -33,41 +31,48 @@ public class MainActivity extends AppCompatActivity {
 
         String textPunto = input.getText().toString();
         if(!textPunto.equals("")){
-            obtenerLiteral(textPunto);
+            getLiteral(textPunto);
         } else {
             setError("Debes ingresar un valor");
         }
+        input.setText("");
     }
 
 
-    private void obtenerLiteral(String textPunto){
+    private void getLiteral(String textPunto){
 
         float punto = Float.parseFloat(textPunto);
 
         if( punto > 100 || punto < 0) {
             setError("El valor deber ser mayor a 0 y menor de 100");
         }else if (punto >= 90 ){
-            setLiteral("A");
+            setLiteral(R.string.literal_A, R.color.green_800, R.string.labelResult_A);
+
         }else if(punto >= 80){
-            setLiteral("B");
+            setLiteral(R.string.literal_B, R.color.yellow_800, R.string.labelResult_B);
+
         }else if(punto >= 70){
-            setLiteral("C");
+            setLiteral(R.string.literal_C, R.color.orange_800, R.string.labelResult_C);
         }else{
-            setLiteral("F");
+            setLiteral(R.string.literal_F, R.color.red_900, R.string.labelResult_F);
         }
     }
 
-    private void setLiteral(String text){
+    private void setLiteral(int text, int color, int textResult){
         result.setText(text);
-        labelResul.setVisibility(View.VISIBLE);
+        result.setTextColor(getColor(color));
         result.setVisibility(View.VISIBLE);
+
+        labelResult.setText(textResult);
+        labelResult.setVisibility(View.VISIBLE);
         labelError.setVisibility(View.INVISIBLE);
+
     }
 
     private void setError(String text){
         labelError.setText(text);
         labelError.setVisibility(View.VISIBLE);
-        labelResul.setVisibility(View.INVISIBLE);
+        labelResult.setVisibility(View.INVISIBLE);
         result.setVisibility(View.INVISIBLE);
     }
 }
